11114cf3ba7ba135846c36aa605efbebeeb34bbb
Jostein Brevik <jostein.brevik@gmail.com>
2020-03-16 20:30:27 +0100

    Add mapmarker, new design for StoreInfo

---
+++ src/assets/MapMarker.vue
@@ -0,0 +1,21 @@
+<template>
+  <img class="marker" v-bind:src="imageSrc" />
+</template>
+
+<script>
+export default {
+  name: "MapMarker",
+  data() {
+    return {
+      imageSrc: require("@/assets/marker.svg")
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.marker {
+  height: 20px;
+  fill: currentColor;
+}
+</style>
--- src/components/StoreInfo.vue
+++ src/components/StoreInfo.vue
@@ -4,25 +4,39 @@
     class="storeInfoContainer"
     v-bind:class="{ open: currentStoreIsOpen, closed: !currentStoreIsOpen }"
   >
-    <h3>
-      {{
-        store.storeName + ", " + this.prettifyDistance(store.distanceFromUser)
-      }}
-    </h3>
-    <p>{{ store.address.street }}</p>
-    <p>Åpen: {{ currentStoreIsOpen ? "Ja!" : "Nei" }}</p>
-    <div class="mapsButton" v-on:click.stop="goToMap">
-      <a> Åpne i Maps</a>
+    <div class="text">
+      <div>
+        <h3>
+          {{
+            store.storeName +
+              ", " +
+              this.prettifyDistance(store.distanceFromUser)
+          }}
+        </h3>
+        <p>{{ store.address.street }}</p>
+      </div>
+
+      <p v-if="currentStoreIsOpen">Stenger {{ getStoreClosingTime }}</p>
+      <p v-else>Stengt</p>
+
+      <!-- <p>Åpen: {{ currentStoreIsOpen ? "Ja!" : "Nei" }}</p> -->
+      <div v-if="this.showAllInfo">
+        <p>Alt: {{ store }}</p>
+      </div>
     </div>
-    <div v-if="this.showAllInfo">
-      <p>Alt: {{ store }}</p>
+
+    <div class="mapContainer" v-on:click.stop="goToMap">
+      <MapMarker />
+      Åpne i kart
     </div>
   </div>
 </template>
 
 <script>
 import { IStore } from "../types/customTypes";
-import { storeIsOpen } from "../mixins/locationMixins";
+import { storeIsOpen, closingTimeToday } from "../mixins/locationMixins";
+import MapMarker from "../assets/MapMarker.vue";
+
 export default {
   props: {
     store: IStore
@@ -31,6 +45,9 @@ export default {
     return {
       showAllInfo: false
     };
+  },
+  components: {
+    MapMarker
   },
   methods: {
     toggleAllInfo: function() {
@@ -57,6 +74,9 @@ export default {
     }
   },
   computed: {
+    getStoreClosingTime() {
+      return closingTimeToday(this.$props.store);
+    },
     currentStoreIsOpen() {
       return storeIsOpen(this.$props.store);
     }
@@ -64,12 +84,16 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .storeInfoContainer {
   padding: 1rem 1.5rem;
-  margin: 3rem 1rem;
+  margin: 3rem 0;
   border-radius: 16px;
   background: linear-gradient(145deg, #242424, #1f1f1f);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 
   // Neumorphism
   border-radius: 32px;
@@ -87,13 +111,34 @@ export default {
   }
 }
 
-.mapsButton {
-  padding: 1rem 2rem;
-  background-color: #eeeeee;
-  color: black;
+p {
+  margin: 5px 0;
+}
+
+h3 {
+  margin: 5px 0;
+}
+
+.text {
+  text-align: left;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.mapContainer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
   cursor: pointer;
-  margin: 1rem;
-  border-radius: 1rem;
-  font-weight: 500;
+  border-radius: 10px;
+  background: #ddd;
+  box-shadow: 0px 0px 5px rgba(200, 200, 200, 0.1);
+  margin-top: 1rem;
+  padding: 0.5rem 0;
+  color: #222;
+  font-weight: 600;
 }
 </style>
