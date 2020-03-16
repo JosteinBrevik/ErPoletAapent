47853ef424bd52fc78a8fa75909e4f020eb0839a
Jostein Brevik <jostein.brevik@gmail.com>
2020-03-16 20:29:51 +0100

    Check time in minutes

--- src/mixins/locationMixins.ts
+++ src/mixins/locationMixins.ts
@@ -19,27 +19,20 @@ const timeToNumbers = (time: string) => {
   return time.split(":").map(num => Number(num));
 };
 
+const timeToMinutes = (time: string) => {
+  const [hours, minutes] = time.split(":");
+  return Number(hours) * 60 + Number(minutes);
+};
+
 export const timeIsBetween = (
   currentTime: string,
   startTime: string,
   endTime: string
 ) => {
-  const [currentHours, currentMinutes] = timeToNumbers(currentTime);
-
-  // console.log("Hours:", currentHours, "Minutes", currentMinutes);
-  const [startHours, startMinutes] = timeToNumbers(startTime);
-  // console.log("StartHours:", startHours, "Minutes", startMinutes);
-  const [endHours, endMinutes] = timeToNumbers(endTime);
-  // console.log("EndHours:", endHours, "Minutes", endMinutes);
-
-  if (currentHours > startHours && currentHours < endHours) {
-    return true;
-  } else if (currentHours === startHours && currentMinutes > startMinutes) {
-    return true;
-  } else if (currentHours === endHours && currentMinutes < endMinutes) {
-    return true;
-  }
-  return false;
+  const currentMinutes = timeToMinutes(currentTime);
+  const startMinutes = timeToMinutes(startTime);
+  const endMinutes = timeToMinutes(endTime);
+  return currentMinutes > startMinutes && currentMinutes < endMinutes;
 };
 
 export const getNow = () => {
@@ -49,14 +42,21 @@ export const getNow = () => {
   return { currentDay: dayOfTheWeek, currentTime: time };
 };
 
+export const closingTimeToday = (store: IStore) => {
+  const today = new Date().getDay();
+  return store.openingHours.regularHours[today];
+};
+
 export const storeIsOpen = (store: IStore) => {
   const { currentDay, currentTime } = getNow();
   const openingHoursToday = store.openingHours.regularHours[currentDay];
   if (!openingHoursToday) {
+    // console.log("no opening hours", store.storeName);
     return false;
   }
 
   if (openingHoursToday.closed) {
+    // console.log("closed", store.storeName);
     return false;
   }
   return timeIsBetween(
